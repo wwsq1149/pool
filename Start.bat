9eda022f3bab8db2990a6228a7c5634790ab1f66
Cat.29 <70135780+wwsq1149@users.noreply.github.com>
2021-11-03 10:55:20 +0800

    Pool
    Pool

--- Start.bat
+++ Start.bat
@@ -1,4 +1,2 @@
-git clone https://github.com/wwsq1149/pool
-D:\a\pool\pool\GetFreeProxies.exe
-D:\a\pool\pool\pf.exe http 15 1000 D:\a\pool\pool\Proxy\http.txt D:\a\pool\pool\Proxy\http-pf.txt
-type D:\a\pool\pool\Proxy\http-pf.txt
+.\GetFreeProxies.exe
+.\pf.exe http 15 1000 .\Proxy\http.txt .\http-pf.txt
