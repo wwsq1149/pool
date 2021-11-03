03e288337f9540853f94326caca0637173ec6e68
Cat.29 <70135780+wwsq1149@users.noreply.github.com>
2021-11-03 11:05:50 +0800

    Pool
    Pool

--- Start.bat
+++ Start.bat
@@ -1,2 +1,11 @@
 .\GetFreeProxies.exe
 .\pf.exe http 15 1000 .\Proxy\http.txt .\http-pf.txt
+
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
+
+git add .\Proxy\HTTP.txt
+git add .\Proxy\HTTP-PF.txt
+
+git commit -m "new commit"
+git push origin main
