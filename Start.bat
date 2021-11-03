c8b80e1dbde87ec54114abc3ede458267f8950d5
Cat.29 <70135780+wwsq1149@users.noreply.github.com>
2021-11-03 10:29:40 +0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,5 +1,4 @@
 git clone https://github.com/wwsq1149/pool
 D:\a\pool\pool\GetFreeProxies.exe
-type D:\a\pool\pool\Proxy\http.txt
 D:\a\pool\pool\pf.exe http 15 1000 D:\a\pool\pool\Proxy\http.txt D:\a\pool\pool\Proxy\http-pf.txt
 type D:\a\pool\pool\Proxy\http-pf.txt
