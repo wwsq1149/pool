ad8d66937375cb32c95443c9a48e16f00c43475b
Cat.29 <70135780+wwsq1149@users.noreply.github.com>
2021-11-03 11:18:33 +0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,11 +1,10 @@
 .\GetFreeProxies.exe
-.\pf.exe http 15 1000 .\Proxy\http.txt .\http-pf.txt
+.\pf.exe http 15 888 .\Proxy\http.txt .\http-pf.txt
 
 git config user.name "GitHub Actions Bot"
 git config user.email "<>"
 
-git add .\Proxy\HTTP.txt
-git add .\Proxy\HTTP-PF.txt
+git add ./
 
 git commit -m "new commit"
 git push origin main
